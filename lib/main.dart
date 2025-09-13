326730dca294e5d2ea118d74e2c22d1e61ff5cd7
NikhilD1998 <nikhildadab@hotmail.com>
2025-09-13 20:33:14 +0530

    Add Firebase integration and configuration for the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:task_manager/firebase_options.dart';
 import 'package:task_manager/screens/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
